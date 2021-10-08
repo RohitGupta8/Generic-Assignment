885e42650bedf346889eb202f1c60b634541499d
RohitGupta8 <therohitg@gmail.com>
2021-10-08 17:36:24 +0530

    ADD : Ability to test Three Maximum String value at desired location

--- src/test/java/com/bl/generic/MaximumValueTest.java
+++ src/test/java/com/bl/generic/MaximumValueTest.java
@@ -41,4 +41,22 @@ public class MaximumValueTest {
         float value = max.findMax(9.0f, 5.0f, 11.0f);
         Assert.assertEquals(11.0f, value, 0.0f);
     }
+
+    @Test
+    public void givenThreeNumberWhenStringValueShouldReturnMaxAtFirstPosition() {
+        String value = max.findMax("peach", "apple", "banana");
+        Assert.assertEquals("peach", value);
+    }
+
+    @Test
+    public void givenThreeNumberWhenStringValueShouldReturnMaxAtSecoundPosition() {
+        String value = max.findMax("apple", "peach", "banana");
+        Assert.assertEquals("peach", value);
+    }
+
+    @Test
+    public void givenThreeNumberWhenStringValueShouldReturnMaxAtThirdPosition() {
+        String value = max.findMax("apple", "banana", "peach");
+        Assert.assertEquals("peach", value);
+    }
 }
