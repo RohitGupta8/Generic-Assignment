4bc2215fb56659a4547fa0f9eca5d361ef948d62
RohitGupta8 <therohitg@gmail.com>
2021-10-08 17:19:48 +0530

    ADD : Ability to find Maximum Float value

--- src/main/java/com/bl/generic/MaximumValue.java
+++ src/main/java/com/bl/generic/MaximumValue.java
@@ -1,6 +1,10 @@
 package com.bl.generic;
 
 public class MaximumValue {
+
+    /**
+     * Find Integer maximum value
+     */
     public Integer findMax(Integer value1, Integer value2, Integer value3) {
         Integer max = value1;
         if (value2.compareTo(max) > 0) {
@@ -10,6 +14,21 @@ public class MaximumValue {
             max = value3;
         }
         System.out.println("Maximum Integer number is " + max);
+        return max;
+    }
+
+    /**
+     * Find float maximum value
+     */
+    public Float findMax(Float value1, Float value2, Float value3) {
+        Float max = value1;
+        if (value2.compareTo(max) > 0) {
+            max = value2;
+        }
+        if (value3.compareTo(max) > 0) {
+            max = value3;
+        }
+        System.out.println("Maximum Float number is " + max);
         return max;
     }
 }
