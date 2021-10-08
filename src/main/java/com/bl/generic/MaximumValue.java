18bc5b8f6332aae693660048e8e1498f188ca4c9
RohitGupta8 <therohitg@gmail.com>
2021-10-08 18:06:30 +0530

    ADD : Refactor Code to generic class

--- src/main/java/com/bl/generic/MaximumValue.java
+++ src/main/java/com/bl/generic/MaximumValue.java
@@ -1,11 +1,16 @@
 package com.bl.generic;
 
-public class MaximumValue {
+public class MaximumValue<T extends Comparable<T>> {
 
-    /**
-     * Generic Method
-     */
-    public <T extends Comparable<T>> T findMax(T value1, T value2, T value3) {
+    T value1, value2, value3;
+
+    public MaximumValue(T value1, T value2, T value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public static <T extends Comparable<T>> T testMaximum(T value1, T value2, T value3) {
         T max = value1;
         if (value2.compareTo(max) > 0) {
             max = value2;
@@ -13,7 +18,12 @@ public class MaximumValue {
         if (value3.compareTo(max) > 0) {
             max = value3;
         }
-        System.out.println("Maximum Integer number is " + max);
+        System.out.println("Maximum value is " + max);
         return max;
     }
+
+    public T findMax() {
+        return MaximumValue.testMaximum(value1, value2, value3);
+    }
+
 }
